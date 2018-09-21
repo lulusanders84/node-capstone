03d5d9398bb8b49d33e723380abf5aa8e9f1a921
Lucy Sanders <lucyhsanders@gmail.com>
2018-09-20 21:44:14 -0500

    added app.get(*) line 23

--- server.js
+++ server.js
@@ -20,6 +20,10 @@ const jwtAuth = passport.authenticate('jwt', {session: false});
 
 app.use(express.static('public'));
 
+app.get('*',(req, res) => {
+    res.sendFile(path.resolve(__dirname, 'public', 'index.html'));
+});
+
 app.use(morgan('common'));
 
 app.use(cors());
