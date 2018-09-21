b457ef0d90960aefc845253a20e34fd169731c77
Lucy Sanders <lucyhsanders@gmail.com>
2018-09-20 21:03:13 -0500

    fixed runServer

--- server.js
+++ server.js
@@ -52,16 +52,18 @@ let server;
 
 function runServer(databaseUrl, port = PORT) {
   return new Promise((resolve, reject) => {
-    mongoose.connect((databaseUrl, err) => {
+    mongoose.connect(
+      databaseUrl,
+      err => {
         if (err) {
           return reject(err);
         }
         server = app
-          .listen(port => {
+          .listen(port, () => {
             console.log(`Your app is listening on port ${port}`);
             resolve(server);
           })
-          .on(err => {
+          .on("error", err => {
             mongoose.disconnect();
             reject(err);
           }
