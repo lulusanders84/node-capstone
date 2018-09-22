ab243cf61f115210eede04c64764041bbc4f158e
Lucy Sanders <lucyhsanders@gmail.com>
2018-09-22 10:43:04 -0500

    patient get endpoint working locally

--- server.js
+++ server.js
@@ -1,25 +1,62 @@
 
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const mongoose = require('mongoose');
+//const morgan = require('morgan');
+//const passport = require('passport');
 
 mongoose.Promise = global.Promise;
 
+const { Patient } = require('./models/patients');
+const { Report } = require('./models/reports');
 const { PORT, DATABASE_URL } = require("./config");
+/*const { router: authRouter } = require("./auth/router");
+const {localStrategy, jwtStrategy } = require("./auth/strategies");
+
+const patientRouter = require('./routes/patient-router');
+const reportRouter = require('./routes/report-router');
+const userRouter = require('./routes/user-router');
+
+const jwtAuth = passport.authenticate('jwt', {session: false});*/
+
+app.use(express.static('public'));
+
+//app.use(morgan('common'));
+
+app.use(cors());
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE');
+  if (req.method === 'OPTIONS') {
+    return res.send(204);
+  }
+  next();
+});
 
 app.get('/api/patients', (req, res) => {
-  Patients
+  Patient
   .find({})
   .then(patients => {
     res.status(200).json(patients)
   })
   .catch(err => {
-    res.status(500).json({message: 'Internal server error'})
+    res.status(500).json({message: 'Internal server error', err: err})
   })
 })
 
+/*passport.use(localStrategy);
+passport.use(jwtStrategy);
+
+app.use('/api/patients', patientRouter);
+app.use('/api/reports', reportRouter, jwtAuth);
+app.use('/api/users', userRouter);
+app.use('/api/auth', authRouter);*/
+
 if (require.main === module) {
-  app.listen(process.env.PORT || 3050, function() {
+  app.listen(process.env.PORT, function() {
     console.info(`App listening on ${this.address().port}`);
   });
 }
