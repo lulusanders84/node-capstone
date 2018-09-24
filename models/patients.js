5687193a9e9c2fcd10614c42d32db65cca92ef08
Lucy Sanders <lucyhsanders@gmail.com>
2018-09-24 16:33:29 -0500

    removed comma in patients model

--- models/patients.js
+++ models/patients.js
@@ -2,8 +2,7 @@
 const mongoose = require("mongoose");
 
 const patientSchema = mongoose.Schema({
-  "report": { type: mongoose.Schema.Types.ObjectId, ref: 'Report' },
-
+  "report": { type: mongoose.Schema.Types.ObjectId, ref: 'Report' }
 });
 
 patientSchema.pre('find', function(next) {
