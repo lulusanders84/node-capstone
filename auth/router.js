673f9a439b77cbc8ef1fc4758d8a8ebe2cbbd377
Lucy Sanders <lucyhsanders@gmail.com>
2018-09-26 17:28:16 -0600

    Testing mongodb response

--- auth/router.js
+++ auth/router.js
@@ -26,17 +26,18 @@ router.post('/login', localAuth, (req, res) => {
   //return;
   const authToken = createAuthToken(req.user.serialize());
   res.send('hello');
-  // User.findOne({ username: req.body.username })
-  // .then(user => {
-  //   res.status(200).json({
-  //     authToken,
-  //     user: {
-  //       "_id": user._id,
-  //       "firstName": user.firstName,
-  //       "assignmentLength": user.assignmentList.length
-  //     }
-  //   });
-  // })
+  User.findOne({ username: req.body.username })
+  .then(user => {
+    res.send(JSON.stringify(user));
+    // res.status(200).json({
+    //   authToken,
+    //   user: {
+    //     "_id": user._id,
+    //     "firstName": user.firstName,
+    //     "assignmentLength": user.assignmentList.length
+    //   }
+    // });
+  })
 
 });
 
